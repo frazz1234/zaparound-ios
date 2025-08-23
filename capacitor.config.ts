644137ec0452e1053e0a87a56713150444da9223
frazz1234 <66176776+frazz1234@users.noreply.github.com>
2025-08-23 09:47:20 -0400

    Add Capacitor plugins and update sitemap last modified dates

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.zaparound.app',
+  appName: 'ZapAround',
+  webDir: 'dist'
+};
+
+export default config;
